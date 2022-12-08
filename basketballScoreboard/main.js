9a0ad3630e5f3e5a39fdefe363bcc0833853de48
Jesses Cemac Ukaegbu <85634164+JessesCUkaegbu@users.noreply.github.com>
2022-12-08 22:32:17 +0000

    fitst commit

---
+++ basketballScoreboard/main.js
@@ -0,0 +1,10 @@
+let score = document.getElementById('score');
+let add = document.getElementById('addOne');
+let addTwo = document.getElementById('addTwo');
+let addThree = document.getElementById('addThree');
+
+function addMe(){
+     add+= 1
+     add.innerHTML = score
+}
+
